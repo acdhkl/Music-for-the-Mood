52263d6837b6727e1b0d87f7803dd05c02bc0c10
acdhkl <dhakalavinav@alumni.ubc.ca>
2020-06-07 17:21:16 -0700

    Added ability to delete, sort songs by genre
    - Delete button appears under every song to remove from DB
    
    - Array of genres hardcoded, songs appear only under their respetive genres on home page.

--- app.js
+++ app.js
@@ -2,42 +2,28 @@ var express = require("express"),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    request = require('request');
+    request = require('request'),
+    methodOverride = require('method-override'),
     Song = require('./models/song.js');
 
 var songRoutes = require('./routes/songs'),
     indexRoutes = require('./routes/index');
 
+
+
 mongoose.connect("mongodb://localhost/song_reccomender", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.set("view engine", "ejs");
 
-
-// Song.create({
-//     name: "Righteous",
-//     image: "https://static.stereogum.com/uploads/2020/04/unnamed-54-1587734342.jpg",
-//     type: "SAD"
-// }, function (err, song) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("New song");
-//         console.log(song);
-//     }
-// })
-
-
-
-
-
-
-
-
+app.use(methodOverride("_method"));
 app.use(indexRoutes);
 app.use(songRoutes);
 
--- routes/songs.js
+++ routes/songs.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Song = require('../models/song.js');
+var genres = ["Workout", "Sad"];
 
 //HOME PAGE- DISPLAY EVERY SINGLE SONG
 router.get("/songs", function (req, res) {
@@ -9,41 +10,51 @@ router.get("/songs", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("songs/index", { songs: allSongs })
+            res.render("songs/index", {
+                songs: allSongs,
+                genres: genres
+            })
         }
     });
 });
 
 //RENDER ADD A NEW SONG PAGE
-router.get('/songs/new', function(req, res) {
-	res.render('songs/new');
+router.get('/songs/new', function (req, res) {
+    res.render('songs/new');
 });
 
 //CREATE
-router.post('/songs', function(req, res) {
-	//get data from form and make a song
-	var name = req.body.name;
-	var image = req.body.image;
-	var type = req.body.type;
-	var newSong = {
-		name: name,
-		image: image,
-		type: type,
-	};
-	// Create a new song and save to DB
-	Song.create(newSong, function(err, newlyCreated) {
-		if (err) {
-			console.log(err);
-		} else {
-			//redirect back to songs home page
-			res.redirect('/songs');
-		}
-	});
+router.post('/songs', function (req, res) {
+    //get data from form and make a song
+    var name = req.body.name;
+    var image = req.body.image;
+    var type = req.body.type;
+    var newSong = {
+        name: name,
+        image: image,
+        type: type,
+    };
+    // Create a new song and save to DB
+    Song.create(newSong, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect back to songs home page
+            res.redirect('/songs');
+        }
+    });
 });
 
-router.post("/songs", function (req, res) {
-    res.render("songs");
-});
+//DESTROY
+router.delete("/songs/:id", function (req, res) {
+    Song.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect('/songs');
+        } else {
+            res.redirect('/songs');
+        }
+    })
+})
 
 
 
